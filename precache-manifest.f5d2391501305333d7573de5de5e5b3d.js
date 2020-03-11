08427f9dffe592c4837075f74e4adce7ec0937b0
Mark Langhamer <langhamerm@gmail.com>
2020-03-10 20:30:06 -0500

    Updates

---
+++ precache-manifest.f5d2391501305333d7573de5de5e5b3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23127bfaba89c5b1b97aace12c9d65d2",
+    "url": "/ClickyGame2/index.html"
+  },
+  {
+    "revision": "c79bdccc8cfd822951ce",
+    "url": "/ClickyGame2/static/css/main.7c629580.chunk.css"
+  },
+  {
+    "revision": "7b703d76e0af591df088",
+    "url": "/ClickyGame2/static/js/2.eb895b35.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ClickyGame2/static/js/2.eb895b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79bdccc8cfd822951ce",
+    "url": "/ClickyGame2/static/js/main.acae7d0a.chunk.js"
+  },
+  {
+    "revision": "26ba0525bf1643383b8e",
+    "url": "/ClickyGame2/static/js/runtime-main.1fee44c9.js"
+  }
+]);
